bc53b5e4288afbf80a7503aa4d875cd4cb9ed3f5
T0thLewis <t0thlewis@gmail.com>
2022-12-12 20:31:40 +0000

    Add buttons to choose from
    Added buttons that can be clicked to choose one of the 3 options
    to play the game.

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
+let playerChoice = '';
 
-playGame();
+const buttons = document.querySelectorAll('button');
 
-//Function to run the game for 5 rounds
 function playGame() {
-
-    for (let i = 0; i < 5; i++) {
-
-        const playerChoice = checkPlayerChoice();
         const computerChoice = getComputerChoice();
 
         console.log(playRound(playerChoice, computerChoice, choices));
         console.log(`Player = ${playerScore} | Computer = ${computerScore}`);
-
-    }
 
     announceResults(playerScore, computerScore);
 
@@ -24,29 +18,16 @@ function playGame() {
 //Determines final outcome and announces the result
 function announceResults(playerScore, computerScore) {
 
-    if (playerScore > computerScore) {
+    if (playerScore === 5) {
         console.log("Congratulations! You win!");
-    } else if (computerScore > playerScore) {
+    } else if (computerScore === 5) {
         console.log("The Computer wins!");
-    } else {
-        console.log("It's a tie!");
     }
-
 }
 
 //Recursive function to make player input case-insensitive and check if input is correct
-function checkPlayerChoice() {
-    let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-
-    for (const x of choices) {
-        if (x === playerChoice) {
-            return playerChoice;
-        }
-    }
-
-    alert("Invalid choice, please input valid choice!");
-    return checkPlayerChoice();
-
+function getPlayerChoice() {
+    playerChoice = this.classList.value;
 }
 
 //Generates computer choice for the game
@@ -56,30 +37,34 @@ function getComputerChoice() {
 }
 
 //The game rule to determine outcome and add a point to the winner
-function playRound(playerChoice, computerChoice, choices) {
+function playRound(playerChoice, computerChoice) {
     const gameRules = ["Paper beats Rock. ", "Rock beats Scissors. ", "Scissors beat paper. "];
     const winner = ["Computer wins!", "Player wins!"];
-    let condition;
 
     if (playerChoice === "rock" && computerChoice === "paper") {
         computerScore++;
-        return condition = gameRules[0].concat(winner[0]);
+        return gameRules[0].concat(winner[0]);
     } else if (playerChoice === "rock" && computerChoice === "paper") {
         playerScore++;
-        return condition = gameRules[0].concat(winner[1]);
+        return gameRules[0].concat(winner[1]);
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
         computerScore++;
-        return condition = gameRules[1].concat(winner[0]);
+        return gameRules[1].concat(winner[0]);
     } else if (playerChoice === "rock" && computerChoice === "scissors") {
         playerScore++;
-        return condition = gameRules[1].concat(winner[1]);
+        return gameRules[1].concat(winner[1]);
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
         computerScore++;
-        return condition = gameRules[2].concat(winner[0]);
+        return gameRules[2].concat(winner[0]);
     } else if (playerChoice === "scissors" && computerChoice === "paper") {
         playerScore++;
-        return condition = gameRules[2].concat(winner[1]);
+        return gameRules[2].concat(winner[1]);
     } else {
-        return condition = "You both chose the same!";
+        return "You both chose the same!";
     }
 }
+
+buttons.forEach( button => {
+    button.addEventListener('click', getPlayerChoice);
+    button.addEventListener('click', playGame);
+});
